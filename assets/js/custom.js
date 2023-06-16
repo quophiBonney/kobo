def2f3e4ef2e5bd8e32aa992c0f2ced640cda104
bonney <quophiclef@gmail.com>
2023-06-16 00:18:29 -0700

    initial commit

---
+++ assets/js/custom.js
@@ -0,0 +1,12 @@
+const preloaderFunction = () => {
+    const preloader = document.querySelector(".preloader-container");
+    const content = document.getElementById("content");
+    content.style.display = "none"
+    setTimeout(()=> {
+        preloader.style.display = "none";
+        content.style.display = "block";
+    }, 3000)
+
+  
+}
+preloaderFunction();
